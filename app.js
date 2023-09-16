18ac13f066c60af2bee40a832d66d1bdc5be5655
raghuvrt29 <raghu.vrt29@gmail.com>
2023-09-16 11:12:10 +0530

    added dotenv to store app key

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const https=require("https");
 const bodyParser=require("body-parser");
 const app=express();
+require("dotenv").config();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -11,7 +12,7 @@ app.get("/",function(req,res){
 
 app.post("/",function(req,res){
     const query=req.body.cityName;
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=c8e2c93ef2dbdb0c97d5aa831c55635d&units=metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+process.env.appid+"&units=metric";
     https.get(url,function(resp){
         resp.on("data",function(data){
             const weatherData=JSON.parse(data);
